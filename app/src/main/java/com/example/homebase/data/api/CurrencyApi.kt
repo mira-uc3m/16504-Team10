71982be48ed4d83d8e4140dadd8e113d9ff5dd6c
mira-uc3m <100589386@alumnos.uc3m.es>
2026-03-10 13:07:59 +0100

    Update app/src/main/java/com/example/homebase/data/api/CurrencyApi.kt

--- app/src/main/java/com/example/homebase/data/api/CurrencyApi.kt
+++ app/src/main/java/com/example/homebase/data/api/CurrencyApi.kt
@@ -8,7 +8,6 @@ import com.example.homebase.BuildConfig
 import com.google.gson.internal.GsonBuildConfig
 
 interface CurrencyApi {
-    // Replace "YOUR_KEY" with a free key from exchangerate-api.com
     @GET("v6/${BuildConfig.CURRENCY_API_KEY}/latest/{base}")
     suspend fun getLatestRates(@Path("base") base: String): Response<CurrencyResponse>
 }
