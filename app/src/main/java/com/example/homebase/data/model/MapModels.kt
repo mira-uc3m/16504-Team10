3bec29dbf77091f38934f9a7f39b768be7604075
mira-uc3m <100589386@alumnos.uc3m.es>
2026-04-13 16:55:50 +0200

    Change highlighted locations for Leganes and Getafe campuses to be the campus buildings

--- app/src/main/java/com/example/homebase/data/model/MapModels.kt
+++ app/src/main/java/com/example/homebase/data/model/MapModels.kt
@@ -9,22 +9,23 @@ data class CampusBuilding(
 )
 
 object CampusData {
-    // Coordinates for Av. de la Universidad, 30, Leganés
+    // Coordinates for UC3M Leganés
     val leganesPos = LatLng(40.3323, -3.7656)
 
-    // Coordinates for C. Madrid, 126, Getafe
+    // Coordinates for UC3M Getafe
     val getafePos = LatLng(40.3045, -3.7258)
 
     val leganesBuildings = listOf(
-        CampusBuilding("Building 7 - Juan Benet", "50 m", LatLng(40.3325, -3.7658)),
-        CampusBuilding("Bank Secaucus", "1,2 km", LatLng(40.3330, -3.7640)),
-        CampusBuilding("Bank 1657 Riverside Drive", "5,3 km", LatLng(40.3310, -3.7680)),
-        CampusBuilding("Bank Rutherford", "70 m", LatLng(40.3320, -3.7650))
+        CampusBuilding("Building 2 - Leonardo da Vinci", "--", LatLng(40.3320, -3.7645)),
+        CampusBuilding("Building 4 - Torres Quevedo", "--", LatLng(40.3330, -3.7665)),
+        CampusBuilding("Building 7 - Juan Benet", "--", LatLng(40.3325, -3.7658)),
+        CampusBuilding("Building 1 - Auditorium (Padre Soler)", "--", LatLng(40.3315, -3.7640))
     )
 
     val getafeBuildings = listOf(
-        CampusBuilding("Library - Getafe", "100 m", LatLng(40.3048, -3.7250)),
-        CampusBuilding("Building 15 - Lopez Aranguren", "200 m", LatLng(40.3040, -3.7260)),
-        CampusBuilding("Student Center", "350 m", LatLng(40.3035, -3.7270))
+        CampusBuilding("Library - María Moliner", "--", LatLng(40.3048, -3.7250)),
+        CampusBuilding("Building 15 - López Aranguren", "--", LatLng(40.3040, -3.7260)),
+        CampusBuilding("Building 14 - Concepción Arenal", "--", LatLng(40.3055, -3.7265)),
+        CampusBuilding("Student Center - Sports Complex", "--", LatLng(40.3030, -3.7240))
     )
 }
